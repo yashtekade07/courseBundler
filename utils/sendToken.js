460da81ecef3b42eda8033c3b0d3e32fecd72192
Deceit <yashtekade07@gmail.com>
2023-06-30 20:26:12 +0530

    logout fixed

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -5,7 +5,7 @@ export const sendToken =(res,user,message,statusCode = 200)=>{
         expires:new Date(Date.now() + 15*24*60*60*1000),
         httpOnly:true,
         secure:true,
-        sameSite:'none',
+        sameSite:"none",
     }
     res.status(statusCode).cookie("token",token,options).json({
         success:true,
